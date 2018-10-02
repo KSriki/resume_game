6be253738d3084c59ad65f937d707551e24bec5e
github username <github email address>
2018-10-01 20:08:42 -0400

    new and show base page for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  # before_action :find_user, only: [:update, :destroy]
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -10,18 +10,28 @@ class UsersController < ApplicationController
     @user = User.new # Should #find_or_create_by() be used here instead?
   end
 
-  def create
+  def show
 
+  end
+
+  def create
+      @user = User.create(user_params)
+      byebug
+      if @user.save
+          redirect_to user_url(@user)
+      else
+          render :new
+      end
   end
 
 
 private
 
   def user_params
-    params.require(:user).permit(:username, :password, :fullname, :id) # Should accepts_nested_attributes be included here for all other user info?
+    params.require(:user).permit(args*) # Should accepts_nested_attributes be included here for all other user info?
   end
 
   def find_user
-    @user = User.find(user_params[:id])
+    @user = User.find(user_params(:id))
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   validates :password, length: {in: 10..16, message: "length should be between 10-16 characters"}
   validates :password, format: { without: /\s/, message: "excludes all whitespace" }
   validates :username, format: { without: /\s/, message: "excludes all whitespace" }
+  validates :username, uniqueness: true
   validates :fullname, format: { with: /\A[\w\s]+\z/, message: "only allows letters" }
 end
