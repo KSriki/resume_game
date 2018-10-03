579d239a2aa317d3b1f13b88677ded304ea919aa
Trevor Jameson <github email address>
2018-10-03 09:49:51 -0400

    commented out CSS styling and JS

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,7 +1,7 @@
-const input = document.querySelector('body')
-
-input.addEventListener('keydown', function(e) {
-  if (e.which === 89 || w.which === 121) {
-    alert('this should link to sessions#new')
-  }
-})
+// const input = document.querySelector('body')
+//
+// input.addEventListener('keydown', function(e) {
+//   if (e.which === 89 || w.which === 121) {
+//     alert('this should link to sessions#new')
+//   }
+// })
