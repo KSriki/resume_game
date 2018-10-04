d7c1cb4e227e608d49fa938ce659b2b329096662
Trevor Jameson <github email address>
2018-10-04 18:14:19 -0400

    styled logout page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get "/analytics", to: "analytics#index"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#logout"
   get "/users/:id/resume_form", to: "users#resume_form"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
