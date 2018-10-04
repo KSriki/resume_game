8770a9ec0b8367214bb457ec1bdaeeb5ae7f1d02
Trevor Jameson <github email address>
2018-10-04 10:46:41 -0400

    added analytics index page, Company class methods, and analytics controller

---
+++ app/controllers/concerns/analytic_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+
+  def index
+    @analytics = {
+      company_sizes: Company.count_all_sizes,
+      company_sectors: Company.count_all_sectors,
+      industry_companies: Industry.count_companies
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  resources :sessions
-  resources :analytics
+
   root "sessions#homepage"
+  get "/analytics", to: "analytics#index"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
