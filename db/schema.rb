b13ca97ae914c121c86febe8c01a8e2b6de01195
Trevor Jameson <github email address>
2020-02-02 22:23:39 -0500

    Swapped login background asset. Removed error msg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_01_210959) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "companies", force: :cascade do |t|
     t.string "company_name"
     t.string "size"
